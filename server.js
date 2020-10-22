c6023f69d817c623294cfd3fe8b29b727aad4181
Nomz <42914692+Nomz420@users.noreply.github.com>
2020-10-22 14:24:15 +0200

    Naprawiłem to gówno - Nomz, 2020

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-//Discord
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
@@ -10,26 +9,22 @@ bot.on("message", (message) => {
   let msg = message.content;
   if (message.author.id == "768193728690126889") return;
   
-  //Commands
-  if (msg.substring(0, 2)=="s!"){ //Command prefix, like !command
+  if (msg.substring(0, 2)=="s!"){
     let str = msg.substring(2, msg.length);
     let args = str.split(' ');
     let cmd = args[0];
     args.splice(0, 2);
     
-    //Commands
-    switch (cmd){ //Commands here
-      case "hi": //Example of a simple command. Use !hi and it says "hi"
-        message.channel.send("hi");
+    switch (msg){  // Odpowiedzi na normalne wiadomości
+      case "witam":
+        message.channel.send("wilkommen");
       break;
     }
-  }
-  //Message replies
-  else if (message.author.id != "768193728690126889"){ //Replace this number with your bot's user ID
-    let channel = message.channel.name; //Channel name
-    
-    if (msg=="witam"){ //If someone says "hello bot",
-      message.channel.send("wilkommen"); //say "hello hooman"
+
+    switch (cmd){  // Komendy
+      case "help":
+        message.channel.send("Tu nic nie ma ¯\_(ツ)_/¯");
+      break;
     }
   }
 });
