17c8756140143ae212e3b5d6e26d7734b2f6e1ee
Nomz <42914692+Nomz420@users.noreply.github.com>
2020-10-22 14:43:02 +0200

    Naprawiłem to gówno - Nomz, 2020

--- server.js
+++ server.js
@@ -7,24 +7,26 @@ bot.on("ready", () => {
 
 bot.on("message", (message) => {
   let msg = message.content;
-  if (message.author.id == "768193728690126889") return;
+  if (message.author.bot) return;
+
+  if(msg == "witam") {
+    message.channel.send("wilkommen")
+  }
   
   if (msg.substring(0, 2)=="s!"){
     let str = msg.substring(2, msg.length);
     let args = str.split(' ');
     let cmd = args[0];
     args.splice(0, 2);
-    
-    if(message.content == "witam") {
-      message.channel.send("wilkommen")
-    }
 
-    switch (cmd){  // Komendy
+    switch (cmd){
       case "help":
         message.channel.send("Tu nic nie ma.");
       break;
     }
   }
 });
+
+// Wiadomość od Nomza: neotral jeśli coś spierdolisz to cie zajebie. Naucz się JS proszę.
 
 bot.login("NzY4MTkzNzI4NjkwMTI2ODg5.X486KQ.8Z0rAPMyEuZfZlakUDmn9RxoQww");
