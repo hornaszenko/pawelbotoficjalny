5aad247f3d4f1452e119e874ec5be4984bcd5fb0
neotral-web <73197115+neotral-web@users.noreply.github.com>
2020-11-17 10:21:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,62 @@ bot.on("ready", () => {
   console.log(bot.user.tag + " is ready!");
 });
 
+s4d.client = new s4d.Discord.Client();
+s4d.client.on('raw', async (packet) => {
+    if (['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) {
+        const guild = s4d.client.guilds.cache.get(packet.d.guild_id);
+        if (!guild) return;
+        const member = guild.members.cache.get(packet.d.user_id) || guild.members.fetch(d.user_id).catch(() => {});
+        if (!member) return;
+        const channel = s4d.client.channels.cache.get(packet.d.channel_id);
+        if (!channel) return;
+        const message = channel.messages.cache.get(packet.d.message_id) || await channel.messages.fetch(packet.d.message_id).catch(() => {});
+        if (!message) return;
+        s4d.client.emit(packet.t, guild, channel, message, member, packet.d.emoji.name);
+    }
+});
+var arguments2, member_xp, command, member_level;
+
+
+s4d.client.login('NzY4MTkzNzI4NjkwMTI2ODg5.X486KQ.8Z0rAPMyEuZfZlakUDmn9RxoQww').catch((e) => s4d.tokenInvalid = true);
+
+s4d.client.on('message', async (s4dmessage) => {
+    if (!((s4dmessage.member).user.bot)) {
+        member_xp = s4d.database.get(String(('xp-' + String(s4dmessage.author.id))));
+        member_level = s4d.database.get(String(('level-' + String(s4dmessage.author.id))));
+        if (!member_xp) {
+            member_xp = 0;
+        } else if (!member_level) {
+            member_level = 0;
+        }
+        s4d.database.set(String(('xp-' + String(s4dmessage.author.id))), (member_xp + 1));
+        member_xp = member_xp + 1;
+        if (member_xp > 100) {
+            s4d.database.set(String(('level-' + String(s4dmessage.author.id))), (member_level + 1));
+            member_level = member_level + 1;
+            s4dmessage.channel.send(String((['Gratulacje,', (s4dmessage.member), 'Wskoczyłeś na poziom', member_level, '!!'].join(''))));
+        }
+        if ((s4dmessage.content) == 's!level') {
+            s4dmessage.channel.send(String(([(s4dmessage.member), ', jesteś obecnie na poziomie ', member_level].join(''))));
+        } else if ((s4dmessage.content) == '!xp') {
+            s4dmessage.channel.send(String(([(s4dmessage.member), ', you need ', 100 - member_xp, ' to jump to level ', member_level + 1].join(''))));
+        }
+    }
+
+});
+
+s4d.client.on('message', async (s4dmessage) => {
+    arguments2 = (s4dmessage.content).split(' ');
+    command = arguments2.splice(0, 1)[0];
+    if (command == '!say') {
+        s4dmessage.channel.send(String('You have written the following content after !say:'));
+        s4dmessage.channel.send(String((arguments2.join(' '))));
+    }
+
+});
+
+s4d;
+
 bot.on("message", (message) => {
   let msg = message.content;
   if (message.author.bot) return;
