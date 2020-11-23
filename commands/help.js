4a7217ef312800641f2b6bddd8dcba55af80fd18
neotral-web <73197115+neotral-web@users.noreply.github.com>
2020-11-23 20:38:18 +0100

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on("ready", () => {
+  console.log(bot.user.tag + " is ready!");
+});
+  
+ if (msg.substring(0, 2)=="now:"){
+    let str = msg.substring(2, msg.length);
+    let args = str.split(' ');
+    let cmd = args[0];
+    args.splice(0, 2);
+  
+ 
+    if (cmd == "help") {
+      let embed = new Discord.MessageEmbed()
+      embed.setColor("#55FFFF")
+      embed.setDescription("Już za niedługo :eyes:")
+      message.channel.send(embed)
+    }
+   
+    else {
+      return;
+    }  
+  }       
+});
+// Wiadomość od Nomza: neotral jeśli coś spierdolisz to cie zajebie. Naucz się JS proszę.
+
+bot.login("NzY4MTkzNzI4NjkwMTI2ODg5.X486KQ.UpuFKklag9t1kjHt1YyiMmYLzWc");
+
