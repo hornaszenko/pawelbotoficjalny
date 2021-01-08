56c1b16fafc2764e99bd087784b3586aae3701b1
neotral-web <73197115+neotral-web@users.noreply.github.com>
2021-01-08 19:30:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -45,8 +45,9 @@ bot.on("message", (message) => {
        let wiadomosc = args.slice(0).join(" ");
        message.delete();
        message.channel = message.client.channels.cache.get('797163683666985055');
-       message.channel = message.client.channels.cache.get('797167770756186153');
+       message.channel.2 = message.client.channels.cache.get('797167770756186153');
        message.channel.send(wiadomosc)
+       message.channel.2.send(wiadomosc)
     }
    
       
