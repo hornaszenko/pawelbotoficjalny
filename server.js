4c57c168f4ef5d3f8d422333e843f5da0d215538
neotral-web <73197115+neotral-web@users.noreply.github.com>
2021-01-08 18:18:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -76,8 +76,7 @@ bot.on("message", (message) => {
     else if (cmd == "zagraj") {
       const args = message.content.slice(6).trim().split(/ +/g);
       let sansgranie = args.slice(0).join(" ");
-      message.react("✅");
-      voiceChannel.join()
+      message.react("✅")
       .then(connection => {
       connection.play(sansgranie);
       });
