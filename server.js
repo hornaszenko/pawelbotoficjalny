86282fdeec446cd441d0fa73956d59ca92528d27
neotral-web <73197115+neotral-web@users.noreply.github.com>
2021-01-08 20:47:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ bot.on("message", (message) => {
        message.channel = message.client.channels.cache.get('797163683666985055');
        message.channel.kraina = message.client.channels.cache.get('797167770756186153');
        message.channel.send(`${message.author.tag}` + `napisał` + ` ` +`**` + wiadomosc + `**`)
-       message.channel.kraina.send(`${message.author.tag}` + `napisał` + ` ` + `**` + wiadomosc + `**`)
+       message.channel.kraina.send(`${message.author.tag}` + ` ` + `napisał:` + ` ` + `**` + wiadomosc + `**`)
     }
    
       
