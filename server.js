6ab6c6df488e14d8d7c436eee75d790abb2ab517
neotral-web <73197115+neotral-web@users.noreply.github.com>
2021-01-22 09:32:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -36,42 +36,7 @@ bot.on("message", (message) => {
       message.channel.send(embed).then(embedMessage => {
       });
     }
-    else if (cmd = "kick") {
-  
-        if (!message.member.hasPermission("KICK_MEMBERS")) {
-            message.channel.send(":x: Nie masz odopowiednich uprawnień!");
-        }
-        else {
-            if (!message.mentions.users.first()){
-                message.channel.send(":x: Nie oznaczyłeś żadnego użytkownika.")
-                return;
-            }
-            let memberid = message.mentions.users.first().id;
-            let member = message.guild.members.cache.get(memberid);
-            if (member) {
-                try {
-                    if (memberid === message.author.id) {
-                        message.channel.send(":x: Nie możesz wyrzucić samego siebie.")
-                        return;
-                    }
-                    if (member.hasPermission("KICK_MEMBERS")) {
-                        message.channel.send(":x: Nie można wyrzucić użytkownika.")
-                        return;
-                    }
-                    await member.kick();
-                    message.channel.send(":white_check_mark:" + "**" + member.user.tag + "** został wyrzucony z serwera.")
-                }
-                catch (err) {
-                    console.log(err);
-                    message.channel.send(":warning: Wystąpił błąd. Zgłoś to do właściciela bota.")
-                }
-            }
-            else {
-                message.channel.send(":x: Taki użytkownik nie istnieje.")
-            }
-        }
-    },
-}
+
     else if (cmd == "legia") {
        message.channel.send("to stara kurwa")
     }
